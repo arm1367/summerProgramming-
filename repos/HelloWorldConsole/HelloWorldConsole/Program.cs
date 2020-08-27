b4c94a9ab92937c7954a7d6ddd2c7076ea60b661
Alejandro Rojas <58356935+arm1367@users.noreply.github.com>
2020-08-26 22:53:18 -0700

    Add files via upload
    Added folders and files for the C# class

---
+++ repos/HelloWorldConsole/HelloWorldConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*WriteLine means that will jump to the next line of code - 
+              Console.WriteLine("Hello World");*/
+
+            Console.Write("Hello ");
+            Console.WriteLine("World");
+            
+            /* the \n stops the window and waits for user input*/
+            
+            Console.WriteLine("\nPress any key to finish");
+            Console.ReadKey();
+        }
+    }
+}
